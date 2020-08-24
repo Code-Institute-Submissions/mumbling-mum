b831b8d58f3119dcf0a216caeae08b21661b6a19
melanie Boardman <melanie_boardman@hotmail.com>
2020-08-24 23:17:11 +0100

    set up  member_profile to be created upon signup

--- members/models.py
+++ members/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from allauth.account.models import EmailAddress
 from django_countries.fields import CountryField
 
 
@@ -8,7 +9,7 @@ from django_countries.fields import CountryField
 class MemberProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # each user can only have one profile thus one to one field, if user is deleted delete profile
-    # default_email_address = user.email # NOT SURE HOW TO GET FROM ALLAUTH??
+    default_email_address = EmailAddress
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
@@ -17,4 +18,4 @@ class MemberProfile(models.Model):
     default_country = CountryField(blank_label='Country', null=True, blank=True)
     
     def __str__(self):
-        return self.user.username
+        return self.user
--- members/views.py
+++ members/views.py
@@ -1,7 +1,28 @@
+from django.db import models
+from allauth.account.models import EmailAddress
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 # User must be logged in to view the profile page.
+from allauth.account.signals import user_signed_up
+from django.contrib.auth.models import User
+from django.dispatch import receiver
 from .models import MemberProfile
+
+@receiver(user_signed_up)
+
+def create_member_profile(sender=User, **kwargs):
+    print('User signed up!!!')
+    MemberProfile.objects.create(
+    user = User.objects.get(username=User.username),
+    #default_email_address = EmailAddress
+    default_phone_number = '',
+    default_street_address1 = '',
+    default_street_address2 = '',
+    default_town_or_city = '',
+    default_postcode = '',
+    default_country = '',
+    )
+    
 
 @login_required
 def member_profile(request):
@@ -10,8 +31,16 @@ def member_profile(request):
 
     template = 'members/member_profile.html'
 
-    return render(request, template)
-
-
+    context = {
+        'member': member_profile.user,
+        'default_email_address' : member_profile.default_email_address,
+        'default_phone_number' : member_profile.default_phone_number,
+        'default_street_address1': member_profile.default_street_address1,
+        'default_street_address2' : member_profile.default_street_address2,
+        'default_town_or_city' : member_profile.default_town_or_city,
+        'default_postcode': member_profile.default_postcode,
+        'default_country': member_profile.default_country,
+    }
+    return render(request, template, context)
 
 # will need to add a view for the order detail later

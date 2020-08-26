cf55daf29594c3bd7565a9008639015053deb444
melanie Boardman <melanie_boardman@hotmail.com>
2020-08-26 15:41:47 +0100

    Added item_detail view, URL and template

--- items/urls.py
+++ items/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.all_items, name='items'),
     path('category/<str:cat>/', views.items_by_category, name='items_by_category'),
+    path('item_detail/<int:item_id>/', views.item_detail, name='item_detail'),
+    
 ]
--- items/views.py
+++ items/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Item, Category
 
 def all_items(request):
+    """ A view to show all items """
     items = Item.objects.all()
     categories = Category.objects.all()
     context = {
@@ -11,6 +12,7 @@ def all_items(request):
     return render(request, 'items/items.html', context)
 
 def items_by_category(request, cat):
+    """ A view to show items filtered by category """
     items = Item.objects.filter(category=cat)
     categories = Category.objects.all()
     filtered = True
@@ -21,3 +23,13 @@ def items_by_category(request, cat):
         'filtered' : filtered,
     }
     return render(request, 'items/items.html', context)
+
+def item_detail(request, item_id):
+    """ A view to show individual item details """ 
+    item = get_object_or_404(Item, pk=item_id)
+    context = {
+        'item' : item,
+    }
+    return render(request, 'items/item_detail.html', context)
+    
+

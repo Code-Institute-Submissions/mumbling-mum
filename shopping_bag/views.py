e6ea8f9594c5e8709d4709c650a6772795b508d6
melanie Boardman <melanie_boardman@hotmail.com>
2020-08-30 17:28:49 +0100

    django success messageadded sing toasts for shopping bag changes

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.shortcuts import get_object_or_404
+from django.contrib import messages
+from items.models import Item
 
 # Create your views here.
 def view_shopping_bag(request):
@@ -8,7 +10,7 @@ def view_shopping_bag(request):
 
 def add_to_shopping_bag(request, item_id):
     """ A view that adds Add the quantity of the specified item to the shopping bag """
-
+    item = Item.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     #return the user to their previous location on the site
     redirect_url = request.POST.get('redirect_url')
@@ -21,15 +23,20 @@ def add_to_shopping_bag(request, item_id):
     # if this item already exists in the shopping_bag increase the quantity by above quantity
     if item_id in list(shopping_bag.keys()):
         shopping_bag[item_id] += quantity
+        # Add pop up message to show item has been added successfully
+        messages.success(request, f'You have added {item.name} to your shopping bag')
     else:
     # otherwise add the item and quantity to the shopping bag dictionary
         shopping_bag[item_id]= quantity
+        # Add pop up message to show item has been added successfully
+        messages.success(request, f'You have added {item.name} to your shopping bag')
     # update the session shopping_bag with the new contents
     request.session['shopping_bag'] = shopping_bag
     return redirect(redirect_url)
 
 def adjust_shopping_bag(request, item_id):
     """ A view to adjust the quantity of an item in the shopping bag """
+    item = Item.objects.get(pk=item_id) # required for success message
     
     quantity = int(request.POST.get('quantity'))
     
@@ -39,17 +46,21 @@ def adjust_shopping_bag(request, item_id):
     # else remove the item from the bag
     if quantity > 0:
         shopping_bag[item_id] = quantity
+        messages.success(request, f'You have adjusted your shopping bag')
     else:
         shopping_bag.pop(item_id)
+        messages.success(request, f'You have removed {item.name} from your shopping bag')
 
     request.session['shopping_bag'] = shopping_bag
     return redirect(reverse(view_shopping_bag))
 
 def remove_from_shopping_bag(request, item_id):
     """ A view to remove an item from the shopping bag"""
+    item = Item.objects.get(pk=item_id) # required for success message
     
     shopping_bag = request.session.get('shopping_bag', {})
     shopping_bag.pop(item_id)
+    messages.success(request, f'You have removed {item.name} from your shopping bag')
 
     request.session['shopping_bag'] = shopping_bag
     return redirect(reverse(view_shopping_bag))

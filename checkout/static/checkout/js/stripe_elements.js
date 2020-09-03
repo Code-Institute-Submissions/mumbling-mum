cea5d74ba74ec928165cbb50fa3d1c4eb8b0160d
melanie Boardman <melanie_boardman@hotmail.com>
2020-09-03 16:15:19 +0100

    Stripe process added

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,9 +1,9 @@
 // get stripe public key and client secret and remove quotation marks
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
 
 // Create a variable using the stripe public key
-var stripe = Stripe(stripe_public_key);
+var stripe = Stripe(stripePublicKey);
 
 // create an instance of stripe elements
 var elements = stripe.elements();
@@ -22,10 +22,67 @@ var style = {
     invalid: {
         color: '#fa755a',
         iconColor: '#fa755a'
-        }
-    };
+    }
+};
 // Use above to create a card element
-var card = elements.create('card', {style: style});
+var card = elements.create('card', {
+    style: style
+});
 
 // mount the card element to the div in checkout html
 card.mount('#card-element');
+
+// add listener event to check for any errors
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html =
+            `<span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span> ${event.error.message}</span>`;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+// Submit the payment to stripe 
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    //  disable the card element and the submit button to prevent multiple submissions
+    card.update({
+        'disabled':
+        true
+    });
+
+    $('#submit-button').attr('disabled', true)
+
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function (result) {
+        if (result.error) {
+            var html =
+            `<span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span> ${event.error.message}</span>`;
+            $(errorDiv).html(html);
+            //  Enable card element and the submit button to allow user to reenter data.
+            card.update({
+                'disabled':
+                false
+            });
+            $('#submit-button').attr('disabled', false)
+        } else {
+            // The payment has been processed!
+            if (result.paymentIntent.status === 'succeeded') {
+               form.submit();
+            }
+        }
+    });
+});
--- checkout/views.py
+++ checkout/views.py
@@ -1,19 +1,41 @@
 from django.shortcuts import render,redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from shopping_bag.contexts import shopping_bag_contents
+
+import stripe
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     shopping_bag = request.session.get('shopping_bag', {})
     if not shopping_bag:
         messages.error(request, "There's nothing in your shopping bag at the moment")
         return redirect(reverse('items'))
 
+    current_shopping_bag = shopping_bag_contents(request)
+    total = current_shopping_bag['grand_total']
+    # stripe requires an integer
+    stripe_total = round(total * 100)
+    # create the payment intent
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    if not stripe_public_key:
+        messages.warning(request, 'Strip public key is missing.\
+            Did you for get to set it in your environment?')
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51HD40YFYmQDcgAXWuHQAxwO7OCu2TzVyQIldUpYL4WwTeiz7XztP1MFkBbNAQVv6N7x1YjbgIrvQAcTvqeNU6DXL00GlY4Zp9e',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
     return render(request, template, context)

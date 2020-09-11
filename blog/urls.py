e95438d0e24663c8b468c9d230d13ea10b3eeea3
melanie Boardman <melanie_boardman@hotmail.com>
2020-09-11 17:21:56 +0100

    Added edit and delete blog entry functionality

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('blog_detail/<int:blogentry_id>/', views.blog_detail, name='blog_detail'),
     path('like/<int:blogentry_id>/', views.like_entry, name='like_entry'),
     path('add_blog_entry/', views.add_blog_entry, name='add_blog_entry'),
-
+    path('manage_blog/', views.manage_blog, name='manage_blog'),
+    path('edit_blog_entry/<int:blogentry_id>', views.edit_blog_entry, name='edit_blog_entry'),
+    path('delete_blog_entry/<int:blogentry_id>', views.delete_blog_entry, name='delete_blog_entry'),
 ]

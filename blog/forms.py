faf6489e2635eead0080c18a78c522cb9ec8244a
melanie Boardman <melanie_boardman@hotmail.com>
2020-09-14 12:56:59 +0100

    Improved Blog detail page and button styling.

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Category, BlogEntry, Comment
+from crispy_forms.helper import FormHelper
 
 class CategoryForm(forms.ModelForm):
     class Meta:
@@ -15,3 +16,11 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model=Comment
         fields='__all__' #include all thefields
+        unlabelled_fields = ('body',)
+    
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_show_labels = True
+        for field in CommentForm.Meta.unlabelled_fields:
+            self.fields[field].label = False
